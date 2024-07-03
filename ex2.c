7b210b1470c5990ee64be38290001e2296cfc1e3
RishikaKotha <rishikakotha999@gmail.com>
2024-07-03 13:04:36 +0530

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,17 @@
+//program to add two numbers
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    a=0;
+    b=0;
+    c=0;
+    printf("enter a number");
+    scanf("%d",&a);
+    printf("enter a number");
+    scanf("%d",&b);
+    c=a+b;
+    printf("sum of two numbers is:%d",c);
+    return 0;
+}
+
