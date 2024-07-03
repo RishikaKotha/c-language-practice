4876042c9e390159c82ba3db9e1787184a89bbd6
RishikaKotha <rishikakotha999@gmail.com>
2024-07-03 12:57:53 +0530

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,16 @@
+//A PROGRAM TO DEMONSTRATE VARIABLE DECLARATION AND INITIALIZATIONS
+#include<stdio.h>
+int main()
+{
+    int rollno=9;
+    char name[]="Rishika";
+    float avg=98.9;
+    char gender='F';
+    double height=5.4;
+    printf("Rollnumber:%d",rollno);
+    printf("\nName=%s",name);
+    printf("\nAverage=%f",avg);
+    printf("\nGender=%c",gender);
+    printf("\nHeight=%lf",height);
+    return 0;
+}
